c5cefb6587ba480e49504e1948d5ecbb8c2a228d
nilachenari <nilachenari@gmail.com>
2023-11-15 09:25:10 +0330

    Add isEven and isOdd methods

--- src/Calculator.java
+++ src/Calculator.java
@@ -53,4 +53,12 @@ public class Calculator {
         }
         return true;
     }
+
+    public static boolean isEven(int number) {
+        return (number % 2 == 0);
+    }
+
+    public static boolean isOdd(int number) {
+        return (number % 2 != 0);
+    }
 }
